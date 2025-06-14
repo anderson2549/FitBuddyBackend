b20fba0df970f0dc234891f2a4f8f4bbfb628a48
anderson2549 <ortizflorezanderson@gmail.com>
2025-06-13 19:25:53 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import '#Config/env.js';
 import httpServer from '#Config/http.js';
 
 const bootstrap = async () => {
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
+    console.log('URL de la base de datos:', process.env.MONGODB_URL);
     await connectDB(process.env.MONGODB_URL);
 
     httpServer.listen(process.env.PORT, () => {
